33be34f60eb71cfb5601c2b89ba0c94ee7540694
HenriqueSD <hsd.foz@gmail.com>
2019-07-25 21:18:03 -0300

    Dynamic style and css class

---
+++ dom/challenge-04/app.js
@@ -0,0 +1,37 @@
+new Vue({
+	el: '#challenge',
+	data: {
+        class1: 'highlight',
+        danger: true,
+        class3: '',
+        class4: '',
+        color5: '',
+        style5: {
+            width: '100px',
+            height: '100px',
+        },
+        width: '0',
+	},
+	methods: {
+		startEffect() {
+            setInterval(() => {
+                this.class1 = this.class1 == 'highlight' ? 'shrink' : 'highlight'
+            }, 1000)
+		},
+		startProgress() {
+            let value = 0
+            const temp = setInterval(() => {
+                value += 5
+                this.width = `${value}%`
+                if (value == 100) clearInterval(temp)
+            }, 500)
+        },
+        setDanger(event) {
+            if (event.target.value == "true") {
+                this.danger = true
+            } else if (event.target.value == "false") {
+                this.danger = false
+            }
+        }
+	}
+})
