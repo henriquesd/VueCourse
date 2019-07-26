06c2f5b74ca0a5c6753df00a2e86338cf7197ef4
henriquesd <hsd.foz@gmail.com>
2019-07-26 08:15:04 -0300

    Lists

---
+++ conditionals-and-lists/challenge-05/app.js
@@ -0,0 +1,17 @@
+new Vue({
+	el: '#challenge',
+	data: {
+        expression: true,
+		names: ['Bruce', 'Ana', 'Rachel', 'Ross'],
+		book: {
+			title: 'Lord of The Rings',
+			author: 'J.R.R. Tolkiens',
+			volume: '3'
+		},
+		student: {
+			id: 10,
+			name: 'Peter',
+			grades: [7.67, 8.33, 6.98, 9.21]
+		}
+	}
+});
