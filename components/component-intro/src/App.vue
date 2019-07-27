ecdef813da3a5edd0338248f64efd72341af96fa
HenriqueSD <hsd.foz@gmail.com>
2019-07-27 20:46:48 -0300

    Components

---
+++ components/component-intro/src/App.vue
@@ -0,0 +1,23 @@
+<template>
+  <div id="app">
+    <h1>Counters</h1>
+    <!-- {{ counter }} -->
+    <app-counter/>
+    <app-counter/>
+    <app-counter/>
+  </div>
+</template>
+
+<script>
+export default {
+  // data() {
+  //   return {
+  //     counter: 0
+  //   }
+  // }
+}
+</script>
+
+<style>
+
+</style>
---
+++ components/component-intro/src/Counter.vue
@@ -0,0 +1,43 @@
+<template>
+    <div class="counter">
+        <span>{{ counter }}</span>
+        <button @click="add">+</button>
+        <button @click="subtract">-</button>
+    </div>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            counter: 0
+        }
+    },
+    methods: {
+        add() {
+            this.counter++
+        },
+        subtract() {
+            this.counter--
+        }
+    }
+}
+</script>
+
+<style>
+    .counter span {
+        border-bottom: 1px solid #CCC;
+        height: 30px;
+        padding: 5px 25px;
+    }
+
+    .counter button {
+        height: 30px;
+        width: 30px;
+        border-radius: 15px;
+        background-color: coral;
+        color: #fff;
+        margin-left: 10px;
+        outline: none;
+    }
+</style>
