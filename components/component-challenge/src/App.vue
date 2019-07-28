217990bc85b493e7a23be110990a666c8aa9fc34
HenriqueSD <hsd.foz@gmail.com>
2019-07-28 19:20:59 -0300

    Component challenge

---
+++ components/component-challenge/src/App.vue
@@ -0,0 +1,27 @@
+<template>
+	<v-app>
+		<app-menu />
+
+		<appToolbar />	
+
+		<v-content>
+			<Carousel />
+		</v-content>
+
+		<app-footer />
+	</v-app>
+</template>
+
+<script>
+import Menu from '@/components/template/Menu.vue'
+import Toolbar from '@/components/template/Toolbar.vue'
+import Carousel from './components/widgets/Carousel.vue'
+import Footer from './components/template/Footer.vue'
+
+export default {
+	components: { appFooter: Footer,
+				  Carousel,
+				  appToolbar: Toolbar,
+				  'app-menu': Menu }
+}
+</script>
---
+++ components/component-challenge/src/components/template/Toolbar.vue
@@ -0,0 +1,18 @@
+<template>
+    <v-toolbar app>
+        <v-toolbar-title class="headline text-uppercase">
+            <span>Challenge</span>
+            <span class="font-weight-light">Component</span>
+        </v-toolbar-title>
+    </v-toolbar>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
