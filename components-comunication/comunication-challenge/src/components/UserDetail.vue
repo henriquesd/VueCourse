ecedf83cfce1abaa2ff59b2a293cdc1c8965e7b7
henriquesd <hsd.foz@gmail.com>
2019-07-29 17:07:45 -0300

    Components comunication challenge

---
+++ components-comunication/comunication-challenge/src/components/UserDetail.vue
@@ -0,0 +1,34 @@
+<template>
+    <div class="user-detail">
+        <h2 v-if="!user">User not selected!</h2>
+        <div v-else>
+            <h2>ID: {{ user.id }}</h2>
+            <h2>Name: {{ user.name }}</h2>
+            <h2>Age: {{ user.age }}</h2>
+        </div>
+    </div>
+</template>
+
+<script>
+import barramento from '@/barramento'
+
+export default {
+    data() {
+        return {
+            user: null
+        }
+    },
+    created() {
+        barramento.onSelectedUser(user => {
+            this.user = user
+        })
+    }
+}
+</script>
+
+<style>
+    .user-detail {
+        flex: 1;
+        border: 1px solid #CCC;
+    }
+</style>
