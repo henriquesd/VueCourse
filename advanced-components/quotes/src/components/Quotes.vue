b9557e02500eb95a1ed70df80184b91a2e95da57
henriquesd <hsd.foz@gmail.com>
2019-07-29 17:17:09 -0300

    Configured quotes (advanced components) project

---
+++ advanced-components/quotes/src/components/Quotes.vue
@@ -0,0 +1,45 @@
+<template>
+    <div class="quotes">
+        <span>
+            <button @click="number--">&lt;</button>
+            <button @click="number++">&gt;</button>
+        </span>
+        {{ quotes[index].text }}
+    </div>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            number: 0,
+            quotes: [{
+                source: 'Entrepreneurs Journal',
+                text: 'Always remember that you are absolutely unique. Just like everyone else.',
+                author: 'Margaret Mead'
+            }, { 
+                source: 'Mother phrases',
+                text: 'This is not a room, its a sty.',
+                author: 'Roberta'
+            }, {
+                source: 'Father phrases',
+                text: 'Ill count to 3! 1, 2, 2...',
+                author: 'Gustavo'
+            }]
+        }
+    },
+    computed: {
+        index() {
+            return Math.abs(this.number % 3)
+        }
+    }
+}
+</script>
+
+<style scoped>
+    .quotes {
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+    }
+</style>
