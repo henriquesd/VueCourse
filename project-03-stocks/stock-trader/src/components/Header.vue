f2d01e7fe94e2d149db1e799e0c3a6b9aa3c1ead
henriquesd <hsd.foz@gmail.com>
2019-08-05 16:45:01 -0300

    Project-03-Stocks

---
+++ project-03-stocks/stock-trader/src/components/Header.vue
@@ -0,0 +1,41 @@
+<template>
+    <v-toolbar app>
+        <v-toolbar-title class="headline text-uppercase mr-4">
+            <span>Stock</span>
+            <span class="font-weight-light">Trader</span>
+        </v-toolbar-title>
+        <v-toolbar-items>
+            <v-btn flat to="/">Home</v-btn>
+            <v-btn flat to="/portfolio">Portfolio</v-btn>
+            <v-btn flat to="/stocks">Actions</v-btn>
+        </v-toolbar-items>
+
+        <v-spacer></v-spacer>
+
+        <v-toolbar-items>
+            <v-btn flat>End Day</v-btn>
+            <v-menu offset-y>
+                <v-btn flat slot="activator">Save & Load</v-btn>
+                <v-list>
+                    <v-list-tile>
+                        <v-list-tile-title>Save Data</v-list-tile-title>
+                    </v-list-tile>
+                     <v-list-tile>
+                        <v-list-tile-title>Load Data</v-list-tile-title>
+                    </v-list-tile>
+                </v-list>
+            </v-menu>
+        </v-toolbar-items>
+        
+    </v-toolbar>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
